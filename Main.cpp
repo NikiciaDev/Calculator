52173e91355bdb7c5edf98d08275ba108a60f53c
NikiciaDev <nikiciayt@gmail.com>
2023-06-03 13:23:34 +0200

    finished code for validating function

--- Main.cpp
+++ Main.cpp
@@ -14,9 +14,14 @@ Number result;
 int main(int argc, int* argv[]) {
 	while (true) {
 		cu::getEquation(equation);
+
+		//erase all spaces from string
+		equation.erase(std::remove_if(equation.begin(), equation.end(), isspace), equation.end());
+
 		if (!mu::verifyEquationValidity(equation)) {
 			cu::abort("Equation seems to be invalid! Aborting!", -1);
 		}
+
 		cu::print("Extracting numbers from equation!");
 		numbers = eu::extractNumbers(equation);
 		cu::print("Extracting operators from equation!");
--- MathUtil.cpp
+++ MathUtil.cpp
@@ -2,6 +2,42 @@
 
 namespace mu {
 	bool verifyEquationValidity(std::string& equation) {
+		bool inNumber = std::isdigit(*equation.begin()) || *equation.begin() == '-';
+		bool hadDot = false, wasLastDot = false, expectingDigit = false;
+
+
+		if (!std::isdigit(*(equation.end() - 1))) return false;
+
+		for (char c : equation) {
+			if (c == '.' && !inNumber) return false;
+
+			if (std::isdigit(c)) {
+				wasLastDot = false;
+				inNumber = true;
+				expectingDigit = false;
+				continue;
+			} else if(!expectingDigit){
+				if (c == '.') {
+					wasLastDot = true;
+					if (!hadDot) {
+						hadDot = true;
+						continue;
+					}
+				} else if (Operator::isValid(c) && inNumber) {
+					inNumber = false;
+					wasLastDot = false;
+					hadDot = false;
+					continue;
+				}
+				if (c == '-') {
+					expectingDigit = true;
+					inNumber = false;
+					continue;
+				}
+			}
+
+			return false;
+		}
 
 		return true;
 	}
